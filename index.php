e2fa330d6328224adee61fedd3914effbc52eaa0
Ananda Feri Setyawan <anandasetyawan123@gmail.com>
2023-08-22 13:48:25 +0700

    commit tugas 1 msib

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MSIB GITS Tugas 1</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+
+    <main class="main-container">
+        <button class="btn" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            My Profile
+        </button>
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body ">
+                <span class="desc-1">My Name Is Feri</span>
+                <span class="desc-2">Informatics Engineering 2021</span>
+                <span class="desc-3">Sultan Agung Islamic University</span>
+                <span class="desc-4">I really like to create concepts and try to make them real because then I feel useful for others.</span>
+            </div>
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
